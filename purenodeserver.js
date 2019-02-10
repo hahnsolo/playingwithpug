ee0f6ccb5434b9162626e8927e8c5f77f6a18417
Chris Hahn-Romanyk <chrishahn88@hotmail.com>
2019-02-10 02:28:59 -0500

    Updated to handle user id's and junk
    Update to handle user id's and also handel bad query params

--- purenodeserver.js
+++ purenodeserver.js
@@ -7,6 +7,8 @@ function handler(req, res) {
 
 const realUrl = url.parse(req.url,true);
 
+res.setHeader('x-server-date', new Date());
+
 // If there is a default path name 
 if(realUrl.pathname === '/')
 {
@@ -29,6 +31,21 @@ else if(realUrl.pathname ==='/hello'){
     //write a request with a name param 
     res.write(`hello ${name}`);
     res.end();
+}
+// allow to process if the url have an id base or name 
+// looks to see if the url starts with /user/
+else if(realUrl.pathname.startsWith('/user/')){
+    // creates a regex to pull out the value from the page after /users/
+    const regex = new RegExp('\/user\/(.+)');
+    const matching = regex.exec(realUrl.pathname);
+    //if there is no value then call a 400 repsonse
+    if(!matching ||!matching[1]){
+        res.writeHead(400, {'content-type':'text-plain'});
+        return res.end();
+    }
+     //write a request with a name param 
+     res.write(`profile of ${matching[1]}`);
+     res.end();
 }
 //catch all if none of the above have been met. display a 400 
 else 
